99dc55dcb8edd46d8ff1860857c2093b3d1d0926
Brendan Tierney <tiernebre@gmail.com>
2022-06-15 22:59:32 -0600

    spacing

--- frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
+++ frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
       <header className="bg-green-500 p-3 text-white">
         <p>Boise Dev Meetup</p>
       </header>
-      <main className="container mx-auto text-center">
+      <main className="container mx-auto text-center py-5">
         <h1 className="text-5xl font-bold mb-5">HELLO BOISE DEVELOPERS AND ENGINEERS!</h1>
         <h2 className="text-2xl font-bold mb-3">I'd like you take to take a survey about the talk so far!</h2>
       </main>
