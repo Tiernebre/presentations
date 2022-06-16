7b5128e29178d60554fb74746ddb812ddae802d8
Brendan Tierney <tiernebre@gmail.com>
2022-06-15 22:54:57 -0600

    headless ui setup

--- frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
+++ frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
@@ -5,7 +5,8 @@ function App() {
         <p>Boise Dev Meetup</p>
       </header>
       <main className="p-5">
-        <h1 className="text-5xl font-bold text-center">HELLO BOISE ENGINEERS!</h1>
+        <h1 className="text-5xl font-bold text-center mb-5">HELLO BOISE ENGINEERS!</h1>
+        <p className="text-center">Are you enjoying the presentation so far?</p>
       </main>
       <footer>
       </footer>
