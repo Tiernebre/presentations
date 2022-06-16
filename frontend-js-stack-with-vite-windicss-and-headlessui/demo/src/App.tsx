01a397a8cd73841fca229b94f5331574346f4f9d
Brendan Tierney <tiernebre@gmail.com>
2022-06-15 22:53:56 -0600

    boise dev meetup

--- frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
+++ frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
@@ -1,7 +1,14 @@
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold text-center">HELLO BOISE ENGINEERS!</h1>
+      <header className="bg-green-500 p-3 text-white">
+        <p>Boise Dev Meetup</p>
+      </header>
+      <main className="p-5">
+        <h1 className="text-5xl font-bold text-center">HELLO BOISE ENGINEERS!</h1>
+      </main>
+      <footer>
+      </footer>
     </>
   )
 }
