4d5da7116951bb4210fe318c4b290cf27676f04c
Brendan Tierney <tiernebre@gmail.com>
2022-06-15 23:27:24 -0600

    visibility

--- frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
+++ frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <h1 className="text-5xl font-bold mb-5">HELLO BOISE DEVELOPERS AND ENGINEERS!</h1>
         <h2 className="text-2xl font-bold mb-3">I'd like you take to take a survey about the talk so far!</h2>
         <SurveyForm />
+        <div className="visible md:hidden">
+          Oh you're on a small viewport device. Awesome!!
+        </div>
       </main>
       <footer>
       </footer>
