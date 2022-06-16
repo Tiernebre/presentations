cb8327872cf452ced3c1066d393317af809d8af8
Brendan Tierney <tiernebre@gmail.com>
2022-06-15 22:55:58 -0600

    enjoying the presentation so far?

--- frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
+++ frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
@@ -4,9 +4,10 @@ function App() {
       <header className="bg-green-500 p-3 text-white">
         <p>Boise Dev Meetup</p>
       </header>
-      <main className="p-5">
-        <h1 className="text-5xl font-bold text-center mb-5">HELLO BOISE ENGINEERS!</h1>
-        <p className="text-center">Are you enjoying the presentation so far?</p>
+      <main className="p-5 text-center">
+        <h1 className="text-5xl font-bold mb-5">HELLO BOISE ENGINEERS!</h1>
+        <h2 className="text-2xl font-bold mb-3">I'd like you take to take a survey about the talk so far!</h2>
+        <p>Are you enjoying the presentation so far?</p>
       </main>
       <footer>
       </footer>
