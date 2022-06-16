cbfb9fbf0291eb2ebfb47dfdd29c2672b6ab9c6a
Brendan Tierney <tiernebre@gmail.com>
2022-06-15 22:58:10 -0600

    hello boise developers and engineers

--- frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
+++ frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
@@ -4,10 +4,9 @@ function App() {
       <header className="bg-green-500 p-3 text-white">
         <p>Boise Dev Meetup</p>
       </header>
-      <main className="p-5 text-center">
+      <main className="container mx-auto text-center">
         <h1 className="text-5xl font-bold mb-5">HELLO BOISE DEVELOPERS AND ENGINEERS!</h1>
         <h2 className="text-2xl font-bold mb-3">I'd like you take to take a survey about the talk so far!</h2>
-        <p>Are you enjoying the presentation so far?</p>
       </main>
       <footer>
       </footer>
---
+++ frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/components/SurveyForm.tsx
@@ -0,0 +1,5 @@
+export const SurveyForm = () => {
+  <form>
+
+  </form>
+}
