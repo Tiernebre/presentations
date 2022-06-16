7a90f7c4052263d6464f34f32f9e398fe9e34926
Brendan Tierney <tiernebre@gmail.com>
2022-06-15 22:56:22 -0600

    hello

--- frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
+++ frontend-js-stack-with-vite-windicss-and-headlessui/demo/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
         <p>Boise Dev Meetup</p>
       </header>
       <main className="p-5 text-center">
-        <h1 className="text-5xl font-bold mb-5">HELLO BOISE ENGINEERS!</h1>
+        <h1 className="text-5xl font-bold mb-5">HELLO BOISE DEVELOPERS AND ENGINEERS!</h1>
         <h2 className="text-2xl font-bold mb-3">I'd like you take to take a survey about the talk so far!</h2>
         <p>Are you enjoying the presentation so far?</p>
       </main>
